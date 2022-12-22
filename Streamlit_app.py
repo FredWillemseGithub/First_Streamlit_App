6412c6ccab93c341dabfd7058e9c33db5be0b573
FredWillemseGithub <121177559+FredWillemseGithub@users.noreply.github.com>
2022-12-22 12:45:42 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,4 +13,7 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+
 streamlit.dataframe(my_fruit_list)
